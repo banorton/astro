fa486438a9ff2a891f48e135a1059af08ecfdae1
Brandon <banorton1@gmail.com>
2024-02-01 11:55:46 -0500

    added gitignore and added parser and tokenizer

---
+++ lexer.py
@@ -0,0 +1,18 @@
+import regex
+
+
+def tokenize(contents):
+    # split contents into tokens
+    lines = contents.split("\n")
+
+    tokens = []
+    for line in lines:
+        currtoken = line.split()
+
+    return tokens
+
+
+def parse(filename):
+    contents = open(filename, "r").read()
+    tokens = tokenize(contents)
+    return tokens
